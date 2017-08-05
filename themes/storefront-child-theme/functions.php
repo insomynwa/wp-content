124b38fe0bf42f66dc4cceecff6768ed71bc3ffa
insomynwa <pikamons@gmail.com>
2017-08-05 19:01:34 +0700

    template functions

--- themes/storefront-child-theme/functions.php
+++ themes/storefront-child-theme/functions.php
@@ -22,6 +22,11 @@ function sf_child_theme_dequeue_style() {
 /**
  * Note: DO NOT! alter or remove the code above this text and only add your custom PHP functions below this text.
  */
+
+require 'inc/dbsnet-template-functions.php';
+// require 'inc/storefront-template-hooks.php';
+// require 'inc/storefront-template-functions.php';
+
 function dbsnettheme_enqueue_styles(){
 	wp_register_style('bootstrap', '/wp-content/themes/' . get_stylesheet() . '/assets/bootstrap/css/bootstrap.min.css');
 	$dependencies = array('bootstrap');
@@ -41,67 +46,17 @@ function dbsnettheme_wp_setup() {
 }
 add_action('after_setup_theme','dbsnettheme_wp_setup');
 
-add_action('storefront_before_site', 'storefront_add_webfunction');
-function storefront_add_webfunction(){
-	?>
-	<?php
-}
-
-function storefront_add_topbar() {
-    ?>
-
-    <?php
-}
-add_action( 'storefront_before_header', 'storefront_add_topbar' );
-
-add_action( 'storefront_header', 'jk_storefront_header_content', 40 );
-function jk_storefront_header_content() { ?>
-	<div style="clear: both; text-align: right;">
-		Have questions about our products? <em>Give us a call:</em> <strong>0800 123 456</strong>
-	</div>
-	<?php
-}
+add_action('storefront_before_site', 'dbsnet_scripts_global',100);
+add_action('storefront_before_site', 'dbsnet_go_top_navigation',100);
+add_action('storefront_before_site', 'dbsnet_header_navigation',100);
 
 add_filter('show_admin_bar','__return_false');
 
 add_action( 'init', 'jk_remove_storefront_header_search' );
+
 function jk_remove_storefront_header_search() {
 	remove_action( 'storefront_header', 'storefront_product_search', 	40 );
 }
-
-/*function dbsnet_product_subcategories($args = array()){
-  $parentid = get_queried_object_id();
-
-  $args = array( 'parent' => $parentid);
-
-  $terms = get_terms('product_cat', $args);
-  if ( $terms ) {
-         
-    echo '<ul class="product-cats">';
-
-    foreach ( $terms as $term ) {
-
-      echo '<li class="category">';                 
-
-      woocommerce_subcategory_thumbnail( $term );
-
-      echo '<h2>';
-      echo '<a href="' .  esc_url( get_term_link( $term ) ) . '" class="' . $term->slug . '">';
-      echo $term->name;
-      echo '</a>';
-      echo '</h2>';
-                                     
-      echo '</li>';
-                                   
-
-    }
-
-    echo '</ul>';
-
-  }
-}
-add_action('woocommerce_before_shop_loop', 'dbsnet_product_subcategories', 100);
-*/
 
 function dbsnet_main_search_query($query){
   if($query->is_search()){
@@ -116,27 +71,23 @@ function dbsnet_main_search_query($query){
   }
   return $query;var_dump($query);die;
 }
+
 add_action('pre_get_posts', 'dbsnet_main_search_query', 1000);
 
 function storefront_recent_products( $args ) {
-
   if ( storefront_is_woocommerce_activated() ) {
     $args = apply_filters( 'storefront_recent_products_args', array(
         'limit'       => 3,
         'columns'       => 3,
         'title'       => __( 'New In', 'storefront' ),
       ) );
-    //echo '<section class="storefront-product-section storefront-best-selling-products" aria-label="Best Selling Products">';
-    //do_action( 'storefront_homepage_before_best_selling_products' );
-    //do_action( 'storefront_homepage_after_best_selling_products_title' );
     echo storefront_do_shortcode( 'recent_products', apply_filters( 'storefront_recent_products_shortcode_args', array(
         'per_page' => intval( $args['limit'] ),
         'columns'  => intval( $args['columns'] ),
       ) ) );
-    //do_action( 'storefront_homepage_after_best_selling_products' );
-    //echo '</section>';
   }
 }
+
 add_shortcode( 'resent_products', 'storefront_recent_products' );
 
 function storefront_best_selling_products( $args ) {
@@ -147,25 +98,25 @@ function storefront_best_selling_products( $args ) {
       'columns' => 3,
       'title'      => esc_attr__( 'Best Sellers', 'storefront' ),
     ) );
-    //echo '<section class="storefront-product-section storefront-best-selling-products" aria-label="Best Selling Products">';
-    //do_action( 'storefront_homepage_before_best_selling_products' );
-    //do_action( 'storefront_homepage_after_best_selling_products_title' );
     echo storefront_do_shortcode( 'best_selling_products', array(
       'per_page' => intval( $args['limit'] ),
       'columns'  => intval( $args['columns'] ),
     ) );
-    //do_action( 'storefront_homepage_after_best_selling_products' );
-    //echo '</section>';
   }
 }
+
 add_shortcode( 'best_sellers', 'storefront_best_selling_products' );
+
 function dbsnet_show_product_loop_sale_flash(){
-  add_filter('woocommerce_sale_flash', 'avia_change_sale_content', 10, 3);
-  
+
+  add_filter('woocommerce_sale_flash', 'dbsnet_change_sale_content', 10, 3);
 }
-function avia_change_sale_content($content, $post, $product){
-return "";
+
+function dbsnet_change_sale_content($content, $post, $product){
+
+  return "";
 }
+
 remove_action('woocommerce_before_shop_loop_item', 'woocommerce_template_loop_product_link_open',10);
 remove_action('woocommerce_shop_loop_item_title', 'woocommerce_template_loop_product_title',10);
 remove_action( 'woocommerce_before_shop_loop_item_title', 'woocommerce_show_product_loop_sale_flash', 10 );
@@ -180,23 +131,25 @@ add_action('woocommerce_after_shop_loop_item', 'dbsnet_template_loop_product_lin
 add_action( 'woocommerce_after_shop_loop_item', 'dbsnet_template_loop_add_to_cart', 10 );
 
 function dbsnet_template_loop_product_link_open(){
-  global $product;//var_dump($product);
+  global $product;
   echo '<span class="thumbnail thumbnail-mini">';
   echo '<a href="' . get_the_permalink() . '" class="woocommerce-LoopProduct-link woocommerce-loop-product__link">';
 }
+
 function dbsnet_template_shop_loop_item_title(){
-  global $product;//var_dump($product);
+  global $product;
   echo '<h4 class="">' . get_the_title() . '</h4>';
 }
+
 function dbsnet_template_loop_price(){
-  global $product;//var_dump($product);
-  //var_dump($product);
+  global $product;
   if ( $price_html = $product->get_price_html() ){
     echo '<p class="price">'. $price_html . '</p>';
   }
   
 }
 function dbsnet_template_loop_product_link_close(){
+
   echo '</a><hr class="line">';
 }
 
@@ -216,7 +169,6 @@ function dbsnet_template_loop_add_to_cart(){
 
     $args = apply_filters( 'woocommerce_loop_add_to_cart_args', wp_parse_args( $args, $defaults ), $product );
 
-    //wc_get_template( 'loop/add-to-cart.php', $args );
     echo apply_filters( 'woocommerce_loop_add_to_cart_link',
       sprintf( '<a rel="nofollow" href="%s" data-quantity="%s" data-product_id="%s" data-product_sku="%s" class="%s">%s</a>',
         esc_url( $product->add_to_cart_url() ),
--- themes/storefront-child-theme/header.php
+++ themes/storefront-child-theme/header.php
@@ -19,63 +19,7 @@
 </head>
 
 <body <?php body_class(); ?>>
-
-	<button type="button" class="btn btn-warning" id="topBtn" title="Go to top">Top</button>
-    <script>
-    	jQuery(document).ready(function($){
-    		$(window).scroll(function(){
-    			scrollFunction();
-    		});
-    		$("#topBtn").click(function(e){
-    			$(document.body).scrollTop(0);
-    			$(document.documentElement).scrollTop(0);
-    		});
-
-    		function scrollFunction(){
-				if($(document.body).scrollTop() > 60 || $(document.body).scrollTop() > 60) {
-			  		$("#topBtn").css('display',"block");
-				}
-				else{
-			  		$("#topBtn").css('display',"none");
-				}
-			}
-    	});
-    </script>
-    <nav class="navbar navbar-inverse navbar-fixed-top">
-      <div class="container">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand" href="<?php _e(home_url());?>">Dibuang Sayang</a>
-        </div>
-        <div id="navbar" class="navbar-collapse collapse">
-          <ul class="nav navbar-nav navbar-right">
-            <?php if(!is_user_logged_in()): ?>
-            <li><a href="#">Daftar</a></li>
-            <li><a href="<?php echo get_permalink( wc_get_page_id( 'myaccount' ) );?>">Masuk</a></li>
-            <?php else: ?>
-            <?php $user_info = get_userdata(get_current_user_id()); ?>
-            <li><a href="<?php echo get_permalink( wc_get_page_id( 'cart' ) );?>"><span class="glyphicon glyphicon-shopping-cart" aria-hidden="true"></span> Keranjang <span class="badge">5</span></a></li>
-            <li class="dropdown">
-              <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><span class="glyphicon glyphicon-th" aria-hidden="true"></span> <?php _e($user_info->first_name); ?> <span class="caret"></span></a>
-              <ul class="dropdown-menu">
-                <li><a href="#"><span class="glyphicon glyphicon-envelope" aria-hidden="true"></span> Kotak Surat <span class="badge">5</span></a></li>
-                <li role="separator" class="divider"></li>
-                <li><a href="<?php echo get_permalink( wc_get_page_id( 'myaccount' ) );?>"><span class="glyphicon glyphicon-home" aria-hidden="true"></span> Profil</a></li>
-                <li><a href="<?php current_user_can('manage_options') ? _e(admin_url()) : _e('#'); ?>"><span class="glyphicon glyphicon-dashboard" aria-hidden="true"></span> Dashboard</a></li>
-                <li role="separator" class="divider"></li>
-                <li><a href="<?php echo esc_url( wc_logout_url( wc_get_page_permalink( 'myaccount' ) ) ); ?>"><span class="glyphicon glyphicon-log-out" aria-hidden="true"></span> Keluar</a></li>
-              </ul>
-            </li>
-            <?php endif; ?>
-          </ul>
-        </div><!--/.navbar-collapse -->
-      </div>
-    </nav>
+<?php do_action( 'storefront_before_site' ); ?>
 
     <div class="container after-navbar bg-warning">
       <div class="row">
