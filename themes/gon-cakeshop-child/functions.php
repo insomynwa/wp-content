484ddb5e18cd6309e5dacfe01b55623c8b0c02c5
insomynwa <pikamons@gmail.com>
2017-09-22 19:31:42 +0700

    Added: Visual Composer code skeleton

--- themes/gon-cakeshop-child/functions.php
+++ themes/gon-cakeshop-child/functions.php
@@ -13,3 +13,13 @@ add_action('wp_enqueue_scripts', 'gon_child_enqueue_styles', 10000);
 function gon_child_enqueue_styles() {
     wp_enqueue_style('gon-child-style', get_stylesheet_directory_uri() . '/style.css' );
 }
+
+// Before VC Init
+add_action( 'vc_before_init', 'vc_before_init_actions' );
+ 
+function vc_before_init_actions() {
+     
+    //var_dump(get_stylesheet_directory());
+    require_once(get_stylesheet_directory().'/vc-elements/vcInfoBox.php' ); 
+     
+}
